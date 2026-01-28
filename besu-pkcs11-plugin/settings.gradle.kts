00a8a5b5ef0930293790ca35f66090ab529255cd
matei2803 <134730237+matei2803@users.noreply.github.com>
2026-01-28 11:16:28 +0200

    Delete besu-pkcs11-plugin directory

--- besu-pkcs11-plugin/settings.gradle.kts
+++
@@ -1,8 +0,0 @@
-// Copyright 2024, Usman Saleem.
-// SPDX-License-Identifier: (Apache-2.0 OR MIT)
-plugins {
-  // Apply the foojay-resolver plugin to allow automatic download of JDKs
-  id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-}
-
-rootProject.name = "besu-pkcs11-plugin"
